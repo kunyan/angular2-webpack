e90d922f4f821b9aa43ba4130facae5a9810bed3
Kun Yan <kyan@redhat.com>
2016-04-15 08:47:41 +0800

    Use mocha replace jasmine

--- test/todo.spec.ts
+++ test/todo.spec.ts
@@ -1,14 +1,18 @@
+/// <reference path="../typings/main.d.ts" />
+import { expect } from 'chai';
+import { assert } from 'chai';
+
 import { Todo } from '../app/todo';
 
 describe('Todo', () => {
 
     it('has text', () => {
         let todo: Todo = {text: 'Washing dishes', done: false};
-        expect(todo.text).toEqual('Washing dishes');
+        expect(todo.text).to.be.equals('Washing dishes');
     });
 
     it('has done', () => {
         let todo: Todo = {text: 'Washing dishes', done: true};
-        expect(todo.done).toEqual(true);
+        expect(todo.done).to.be.equals(true);
     });
 });
